38a12db638ddd09c75a35b41879a98029a2e565c
Dinorush <onparnorthstar@gmail.com>
2025-12-06 19:00:17 -0500

    Match old GUID

--- NoInterruptions/EntryPoint.cs
+++ NoInterruptions/EntryPoint.cs
@@ -4,7 +4,7 @@ using HarmonyLib;
 
 namespace NoInterruptions
 {
-    [BepInPlugin("hirnukuono." + MODNAME, MODNAME, "0.1.5")]
+    [BepInPlugin(MODNAME, MODNAME, "0.1.5")]
     internal sealed class EntryPoint : BasePlugin
     {
         public const string MODNAME = "NoInterruptions";
