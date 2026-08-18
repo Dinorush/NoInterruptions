596ec84b7de43a1f6f61e663ec078518a6ab179a
Dinorush <onparnorthstar@gmail.com>
2026-08-18 17:43:21 -0400

    Fix mine interaction

--- NoInterruptions/EntryPoint.cs
+++ NoInterruptions/EntryPoint.cs
@@ -5,7 +5,7 @@ using NoInterruptions.Patches;
 
 namespace NoInterruptions
 {
-    [BepInPlugin(MODNAME, MODNAME, "0.1.11")]
+    [BepInPlugin(MODNAME, MODNAME, "0.1.12")]
     [BepInDependency("dev.gtfomodding.gtfo-api", BepInDependency.DependencyFlags.HardDependency)]
     internal sealed class EntryPoint : BasePlugin
     {
---
+++ NoInterruptions/Patches/MinePatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace NoInterruptions.Patches
+{
+    [HarmonyPatch(typeof(MineDeployerFirstPerson))]
+    internal static class MinePatch
+    {
+        [HarmonyPatch(nameof(MineDeployerFirstPerson.OnStickyMineSpawned))]
+        [HarmonyPostfix]
+        private static void OnStickyMineSpawned(ISyncedItem item)
+        {
+            MineDeployerInstance mineDeployerInstance = item.Cast<MineDeployerInstance>();
+            if (mineDeployerInstance != null)
+                mineDeployerInstance.PickupInteraction.transform.Translate(0, 0, -0.01f);
+        }
+    }
+}
